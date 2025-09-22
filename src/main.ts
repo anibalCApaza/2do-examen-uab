7c0a2ad83bb424e97b1e3e3d99aafbdc649ec3b8
arca fedora <arcacorini@gmail.com>
2025-09-22 12:39:30 -0400

    feat(pwa): enable service worker and web manifest

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { register as registerSwiperElements } from 'swiper/element/bundle';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerSwiperElements();
 
@@ -12,7 +14,11 @@ const configWithRouter = {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
-    provideRouter(routes, withHashLocation()), // << Hash routing activado
+    provideRouter(routes, withHashLocation()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }), // << Hash routing activado
   ],
 };
 
